3f0c99f5476b06f95d832b01bae97f5d80541ba6
Miguel <miguelarrieta02@gmail.com>
2024-09-19 18:25:20 +0100

    Removed test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,14 +1,1 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:inventory_app/main.dart';
-
-void main() {
-  testWidgets('TODO', (WidgetTester tester) async {
-    await tester.pumpWidget(
-      const ProviderScope(
-        child: MyApp(),
-      ),
-    );
-  });
-}
+void main() {}
