15f9438122940b96e9dc92b197ccbcaf17d6cea0
Miguel <miguelarrieta02@gmail.com>
2024-09-23 11:00:19 +0100

    Started migrating to using appwrite by changing the Item model class

--- lib/data/models/item.dart
+++ lib/data/models/item.dart
@@ -1,20 +1,39 @@
-import 'package:copy_with_extension/copy_with_extension.dart';
-import 'package:equatable/equatable.dart';
+class _ItemFieldNames {
+  static const String id = '\$id';
+  static const String name = 'name';
+  static const String category = 'category';
+  static const String location = 'location';
+  static const String quantity = 'quantity';
+}
 
-part 'item.g.dart';
-
-@CopyWith()
-final class Item extends Equatable {
+class Item {
+  final String id;
   final String name;
   final String category;
   final String location;
+  final int quantity;
 
   const Item({
+    required this.id,
     required this.name,
     required this.category,
     required this.location,
+    required this.quantity,
   });
 
-  @override
-  List<Object?> get props => [name, category, location];
+  Map<String, dynamic> toJson() => {
+        _ItemFieldNames.id: id,
+        _ItemFieldNames.name: name,
+        _ItemFieldNames.category: category,
+        _ItemFieldNames.location: location,
+        _ItemFieldNames.quantity: quantity,
+      };
+
+  factory Item.fromJson(Map<String, dynamic> doc) => Item(
+        id: doc[_ItemFieldNames.id],
+        name: doc[_ItemFieldNames.name],
+        category: doc[_ItemFieldNames.category],
+        location: doc[_ItemFieldNames.location],
+        quantity: doc[_ItemFieldNames.quantity],
+      );
 }
