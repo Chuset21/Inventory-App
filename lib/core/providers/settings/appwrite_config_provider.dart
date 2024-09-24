4854cbf2d960d6c8bc5d1ad87fc60e458d1dd69b
Miguel <miguelarrieta02@gmail.com>
2024-09-24 16:56:03 +0100

    Renamed AppwriteConfigNotifier function with wrong name

--- lib/core/providers/settings/appwrite_config_provider.dart
+++ lib/core/providers/settings/appwrite_config_provider.dart
@@ -1,12 +1,10 @@
-// lib/providers/appwrite_config_provider.dart
-
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:inventory_app/data/models/models.dart';
 
 class AppwriteConfigNotifier extends StateNotifier<AppwriteConfig> {
   AppwriteConfigNotifier(super.initialState);
 
-  void updateSafeDelete(AppwriteConfig value) {
+  void updateConfig(AppwriteConfig value) {
     state = value;
     // TODO: local storage
   }
